417af6f8d088da831a6bdfe980808f9ee99a6712
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-27 15:50:51 -0500

    working python

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,9 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    if len(array) < min_size:
+        delta = min_size - len(array)
+        while delta > 0:
+            array.append(value)
+            delta -= 1
+    return(array)
+#print(pad([1,2,3], 5))
