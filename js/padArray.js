6792a3ac5af070a0d9e8f2ba9959ae217be1e628
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-27 15:24:42 -0500

    working js

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,16 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-
+    //function pad(array, size, padding){	
+        if (value === null){	
+          value = 'None';	
+        }	
+        if (array.length < minSize){	
+          let delta = (minSize - array.length);	
+          while (delta > 0){	
+            array.push(value);	
+            delta --;	
+          }	
+        }	
+        return array;	
 }
+console.log(pad([1,2,3], 3))
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,9 @@
 // Write unit tests!
+
+var pad = require("./padArray");
+//var shallowEqualArrays = require('shallow-equal/arrays');
+
+console.log(pad.pad([1,2,3], 5),JSON.stringify([ 1, 2, 3, 'None', 'None' ]));
+console.log(pad.pad([1,2,3], 5, 'apple'), JSON.stringify([ 1, 2, 3, 'apple', 'apple' ]));
+console.log(pad.pad([1,2,3], 3), JSON.stringify([ 1, 2, 3 ]));
+console.log(pad.pad([1,2,3], 0), JSON.stringify([ 1, 2, 3 ]));
